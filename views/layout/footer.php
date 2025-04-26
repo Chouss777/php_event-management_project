9e6f3b45c7532c4b4f39bff002d1703946098483
Chouss777 <chabanioussama777@gmail.com>
2025-04-26 16:09:48 +0100

    Add files via upload

---
+++ views/layout/footer.php
@@ -0,0 +1,6 @@
+</main>
+    <footer>
+        <p>© <?= date('Y'); ?> - Gestion des Événements | Université Sultan Moulay Slimane</p>
+    </footer>
+</body>
+</html>
---
+++ views/layout/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion des Événements</title>
+    <link rel="stylesheet" href="/php_event-management_project/public/css/style.css?v=2.0">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="/php_event-management_project/views/events/create_event.php">📅 Créer un Événement</a>
+            <a href="/php_event-management_project/views/events/list_events.php">📋 Liste des Événements</a>
+            <a href="/php_event-management_project/views/participants/register_participant.php">👥 Inscrire un Participant</a>
+            <a href="/php_event-management_project/views/inscriptions/list_inscriptions.php">📑 Inscriptions</a>
+        </nav>
+    </header>
+    <main>
